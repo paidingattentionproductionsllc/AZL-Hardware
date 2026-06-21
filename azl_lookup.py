a3837fa16dd5a3065611250af38da4ea8bf27704
KC Casteel <paidingattentionproductions@gmail.com>
2026-06-21 13:45:20 -0400

    Create azl_lookup.py for AZL record lookup
    Add azl_lookup.py for looking up AZL records by number

---
+++ azl_lookup.py
@@ -0,0 +1,10 @@
+# azl_lookup.py
+import sys, json
+from azl_core import azl_record
+
+if len(sys.argv) < 2:
+    print("usage: python azl_lookup.py <n>")
+    sys.exit(1)
+
+n = int(sys.argv[1])
+print(json.dumps(azl_record(n), indent=2))
